009f3a123311e85d1e353df67668b3c556ea4f72
ShindeBhavesh <140695719+ShindeBhavesh@users.noreply.github.com>
2026-03-14 16:06:35 +0530

    Add files via upload

---
+++ NumberPattern.java
@@ -0,0 +1,20 @@
+public class NumberPattern {
+    public static void main(String[] args) {
+        int n = 4;
+        int size = 2 * n - 1; 
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+
+                int top = i;
+                int left = j;
+                int bottom = size - 1 - i;
+                int right = size - 1 - j;
+
+                int minDistance = Math.min(Math.min(top, bottom), Math.min(left, right));
+
+                System.out.print((n - minDistance) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
